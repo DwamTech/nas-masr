76cdacd3f490eee06fc45bbcc36eb42ed713b3e4
Abdelrhman0101 <abdalrhman.software.eg@gmail.com>
2026-03-17 22:04:10 +0200

    asd

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,14 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
     |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    | Here you may configure CORS settings for your application. This
+    | configuration is used by the CORS middleware to determine what
+    | cross-origin requests should be allowed.
     |
     */
 
@@ -19,16 +16,30 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://192.168.1.22:3000', 'http://localhost:3000', 'http://192.168.119.1:3000','https://www.nasmasr.app'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:8000',
+        'http://192.168.1.35:3000',
+        'http://172.29.0.1:3000',      // ✅ Docker Frontend IP
+        'http://192.168.1.60:3001',
+        'http://192.168.1.15:3000',
+        'http://localhost:3000'
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        // '#^https://.*\.example\.com$#',
+    ],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => [
+        'Authorization',
+        'X-Total-Count',
+        'X-Page-Count',
+    ],
 
     'max_age' => 0,
 
     'supports_credentials' => true,
-
 ];
